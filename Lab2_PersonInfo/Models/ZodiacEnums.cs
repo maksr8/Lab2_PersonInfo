2495a5527f62815d161f9f17866d3beb0730809f
Maksym Rozumiei <maksimrozum174@gmail.com>
2025-03-29 22:21:59 +0200

    Add Person model

---
+++ Lab2_PersonInfo/Models/ZodiacEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_BirthDateWindow.Models
+{
+    public enum ChineseZodiac
+    {
+        Monkey, Rooster, Dog, Pig, Rat, Ox,
+        Tiger, Rabbit, Dragon, Snake, Horse, Goat
+    }
+}
