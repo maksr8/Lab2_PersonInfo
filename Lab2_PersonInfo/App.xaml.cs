368bb0d63b19cd602fa84c7d406addf12a11c381
Maksym Rozumiei <maksimrozum174@gmail.com>
2025-03-29 18:19:10 +0200

    Add project files.

---
+++ Lab2_PersonInfo/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab2_PersonInfo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
